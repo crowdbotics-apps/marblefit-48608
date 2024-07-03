2459ad9634853511176f84d15d47aa3dc46e4ace
Crowdbotics <team@crowdbotics.com>
2024-07-03 00:50:29 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "marblefit-48608",
+  url: "https://marblefit-48608.botics.co",
+  api: "https://marblefit-48608.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
